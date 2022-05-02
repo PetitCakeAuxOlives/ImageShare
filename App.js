05a66229da4dde34d3870ff0d257ff2399a9a3c9
PetitCakeAuxOlives <morgan.rdr@gmail.com>
2022-05-02 15:17:03 +0200

    Appli fonctionnel

--- App.js
+++ App.js
@@ -1,14 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import { useState } from 'react';
+
+import dummy from './assets/images/dummy.png';
+import { Platform } from 'expo-modules-core';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const openSharing = async () => {
+   const canIShare = await Sharing.isAvailableAsync();
+   console.log('canIShare:', canIShare);
+   console.log(Platform);
+   if (Platform.OS !== 'Web'){
+     await Sharing.shareAsync(image);
+   }
+   else {
+     alert('You can not share on your platform !')
+   }
+  }
+
+  const openImagePicker = async () => {
+
+    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log(permission);
+    if (permission === false) {
+      alert ('Permission to access your library is required !')
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+    if(pickerResult.cancelled){
+      return;
+  }
+  setImage(pickerResult.uri);
+}
+
+if (image !== null){
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={ {uri: image}  } style={ {width: 300, height: 300 }} />
+
+      <Text style={ {color: '#888', fontSize: 18} }>To share a photo please press the button below </Text>
+      <TouchableOpacity
+        onPress={ openSharing } 
+        style={ styles.button }>
+      <Text style={ styles.textWhite }>Share your photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={ () => { setImage(null) } } 
+        style={ styles.button }>
+      <Text style={ styles.textWhite }>Cancel</Text>
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
+  return (
+    <View style={styles.container}>
+      <Image source={ dummy } style={ {width: 300, height: 300 }} />
+
+      <Text style={ {color: '#888', fontSize: 18} }>To share a photo please press the button below </Text>
+      <TouchableOpacity
+        onPress={openImagePicker} 
+        style={ styles.button }>
+      <Text style={ styles.textWhite }>Pick a photo</Text>
+      </TouchableOpacity>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +86,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10
+  },
+  textWhite: {
+    color: '#FFF',
+  },
+
+
 });
